6827757939d143e3cd7739cd2e555e913867c68c
camiloriosf <camilo.rios.f@gmail.com>
2017-06-06 10:57:06 -0400

    avances 05-06

--- components/MainLayout.js
+++ components/MainLayout.js
@@ -6,35 +6,47 @@ import Header from 'grommet/components/Header';
 import Title from 'grommet/components/Title';
 import Box from 'grommet/components/Box';
 import Anchor from 'grommet/components/Anchor';
+import Animate from 'grommet/components/Animate';
+import { connect } from 'react-redux';
 
-export default props => (
-  <div>
-    <Head>
-      <title>{props.title}</title>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      <link href="//cdnjs.cloudflare.com/ajax/libs/grommet/1.4.0/grommet.min.css" rel="stylesheet" type="text/css" />
-      <link href="/static/theme.css" rel="stylesheet" type="text/css" />
-      { /* <script src="/static/tawk.js" />*/}
-      <script src="/static/smartsupp.js" type="text/javascript" />
-    </Head>
-    <App centered={false}>
-      <Header fixed float justify="between" pad={{ horizontal: 'medium' }} style={props.pathname === '/' ? { backgroundColor: '#434343' } : { backgroundColor: '#434343' }} >
-        <Link prefetch href="/">
-          <Anchor onClick={() => props.changeSection({ section: 0 })} style={{ textDecoration: 'none', color: '#fff' }}>
-            <Title>
-              BitterSweet
-            </Title>
-          </Anchor>
-        </Link>
-        <Box flex justify="end" direction="row" responsive wrap>
-          <Link prefetch href="/quote"><Anchor label="QUOTE" style={props.pathname === '/quote' ? { marginRight: 30, color: '#fff', textDecoration: 'underline' } : { marginRight: 30, color: '#fff', textDecoration: 'none' }} /></Link>
-          <Link prefetch href="/services"><Anchor label="SERVICES" style={props.pathname === '/services' ? { marginRight: 30, color: '#fff', textDecoration: 'underline' } : { marginRight: 30, color: '#fff', textDecoration: 'none' }} /></Link>
-          <Link prefetch href="/about"><Anchor label="ABOUT" style={props.pathname === '/about' ? { marginRight: 30, color: '#fff', textDecoration: 'underline' } : { marginRight: 30, color: '#fff', textDecoration: 'none' }} /></Link>
-          <Link prefetch href="/contact"><Anchor label="CONTACT" style={props.pathname === '/contact' ? { color: '#fff', textDecoration: 'underline' } : { color: '#fff', textDecoration: 'none' }} /></Link>
-        </Box>
-      </Header>
-      {props.children}
-    </App>
-  </div>
-  );
+function mapStateToProps(state) {
+  return {
+    header: state.index.header,
+  };
+}
+
+export default connect(mapStateToProps, null)(
+  ({ children, title, pathname, header }) =>
+    (
+      <div>
+        <Head>
+          <title>{title}</title>
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+          <link href="//cdnjs.cloudflare.com/ajax/libs/grommet/1.4.0/grommet.min.css" rel="stylesheet" type="text/css" />
+          <link href="/static/theme.css" rel="stylesheet" type="text/css" />
+          <script src="/static/zendesk.js" type="text/javascript" />
+        </Head>
+        <App centered={false}>
+          <Animate visible={header} enter={{ animation: 'slide-down', duration: 100, delay: 0 }} leave={{ animation: 'slide-up', duration: 500, delay: 0 }} keep>
+            <Header fixed float justify="between" pad={{ horizontal: 'medium' }} style={pathname === '/' ? { backgroundColor: 'transparent' } : { backgroundColor: '#434343' }} >
+              <Link prefetch href="/">
+                <Anchor style={{ textDecoration: 'none', color: '#fff' }}>
+                  <Title>
+                    BitterSweet
+                  </Title>
+                </Anchor>
+              </Link>
+              <Box flex justify="end" direction="row" responsive wrap>
+                <Link prefetch href="/quote"><Anchor label="QUOTE" style={pathname === '/quote' ? { marginRight: 30, color: '#fff', textDecoration: 'underline' } : { marginRight: 30, color: '#fff', textDecoration: 'none' }} /></Link>
+                <Link prefetch href="/services"><Anchor label="SERVICES" style={pathname === '/services' ? { marginRight: 30, color: '#fff', textDecoration: 'underline' } : { marginRight: 30, color: '#fff', textDecoration: 'none' }} /></Link>
+                <Link prefetch href="/about"><Anchor label="ABOUT" style={pathname === '/about' ? { marginRight: 30, color: '#fff', textDecoration: 'underline' } : { marginRight: 30, color: '#fff', textDecoration: 'none' }} /></Link>
+                <Link prefetch href="/contact"><Anchor label="CONTACT" style={pathname === '/contact' ? { color: '#fff', textDecoration: 'underline' } : { color: '#fff', textDecoration: 'none' }} /></Link>
+              </Box>
+            </Header>
+          </Animate>
+          {children}
+        </App>
+      </div>
+  ),
+);
